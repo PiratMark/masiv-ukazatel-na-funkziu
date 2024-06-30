52327a650122fa71ee0d28d68a71b96daa24eb6b
Компьютер <@DESKTOP-JR5U4KS>
2024-06-30 18:21:26 +0500

    Добавьте файлы проекта.

---
+++ masiv ukazatel na  funkziu/masiv ukazatel na  funkziu.cpp
@@ -0,0 +1,35 @@
+﻿#include <cmath>
+#include <iostream>
+#include <functional>
+
+double a(double x)
+{
+    return sqrt(x);
+}
+
+double b(double x)
+{
+    return x * x * x;
+}
+
+double c(double x)
+{
+    return sin(x);
+}
+
+int main()
+{
+    std::function<double(double)> funcs[] = { a, b, c };
+
+    double x;
+    std::cout << "Enter x: ";
+    std::cin >> x;
+    unsigned int ch, len = sizeof(funcs) / sizeof(*funcs);
+    do
+    {
+        std::cout << "Choose function: ";
+        std::cin >> ch;
+    } while (ch < 1 || ch > len);
+
+    std::cout << "Result: " << funcs[ch - 1](x) << std::endl;
+}
